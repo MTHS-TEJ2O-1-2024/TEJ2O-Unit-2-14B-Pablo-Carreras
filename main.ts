43db37b3de3c9f9ae2158f71f067dd6dd9d54d5d
Pablo-Carreras <pablo.carreras-canton@mths.ca>
2024-11-01 11:08:17 -0400

    made the code

--- main.ts
+++ main.ts
@@ -1,8 +1,60 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Pablo Carreras
+ * Created on: Oct 2024
+ * This program makes a square aroubnd the micro bit 
 */
 
-basic.showString('Hello, World!')
+// variables
+let sprite: game.LedSprite = null
+let loopCounter = 0
+
+// setup
+basic.showIcon(IconNames.Happy)
+basic.pause(500)
+basic.clearScreen()
+
+// When "A" is pressed
+input.onButtonPressed(Button.A, function () {
+    // setup
+    basic.clearScreen()
+    loopCounter = 0
+    sprite = game.createSprite(0, 0)
+
+    loopCounter = 0
+    while (loopCounter <= 5) {
+        sprite.set(LedSpriteProperty.X, loopCounter)
+        loopCounter = loopCounter + 1
+        pause(500)
+    }
+
+    loopCounter = 0
+    while (loopCounter <= 5) {
+        sprite.set(LedSpriteProperty.Y, loopCounter)
+        loopCounter = loopCounter + 1
+        pause(500)
+    }
+
+    loopCounter = 5
+    while (loopCounter >= 0) {
+        sprite.set(LedSpriteProperty.X, loopCounter)
+        loopCounter = loopCounter - 1
+        pause(500)
+    }
+
+    loopCounter = 5
+    while (loopCounter >= 0) {
+        sprite.set(LedSpriteProperty.Y, loopCounter)
+        loopCounter = loopCounter - 1
+        pause(500)
+    }
+
+    loopCounter = 5
+    while (loopCounter > 0) {
+        sprite.set(LedSpriteProperty.X, loopCounter)
+        loopCounter = loopCounter - 1
+    }
+
+    sprite.delete()
+    basic.showIcon(IconNames.Happy)
+})
